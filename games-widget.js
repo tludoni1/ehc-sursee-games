64e1e19766d3887b9fac2b4ffb2a4d2dd90cd246
tludoni1 <tludoni1@users.noreply.github.com>
2025-08-23 11:19:32 +0200

    Update games-widget.js

--- games-widget.js
+++ games-widget.js
@@ -1,4 +1,31 @@
-(function() {
-  console.log("Widget geladen!");
-  document.body.innerHTML += "<h1 style='color:red'>Test Widget funktioniert</h1>";
+(function () {
+  async function renderGames() {
+    try {
+      const res = await fetch("https://tludoni1.github.io/ehc-sursee-games/games-all.json");
+      const games = await res.json();
+
+      console.log("Geladene Spiele:", games.length);
+
+      let html = "<h2>EHC Sursee Spiele</h2>";
+      html += "<ul style='font-family: Arial, sans-serif; line-height:1.6'>";
+
+      games.forEach(g => {
+        html += `
+          <li>
+            ${g.date} | ${g.team1?.name} vs. ${g.team2?.name} 
+            (${g.displayInfo})
+          </li>
+        `;
+      });
+
+      html += "</ul>";
+
+      document.body.innerHTML += html;  // direkt ins body für Test
+    } catch (err) {
+      console.error("Fehler beim Rendern:", err);
+      document.body.innerHTML += "<p style='color:red'>Fehler beim Laden der Spiele!</p>";
+    }
+  }
+
+  renderGames();
 })();
